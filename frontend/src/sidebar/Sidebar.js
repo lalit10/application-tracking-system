1383c151876000653014126c160c2ba3b3cbddcf
Brad Erickson <bbwe24@gmail.com>
2021-11-28 13:03:35 -0500

    Added login page and logout functionality

--- frontend/src/sidebar/Sidebar.js
+++ frontend/src/sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
 import '@fortawesome/fontawesome-free/js/solid';
@@ -7,15 +8,53 @@ import '@fortawesome/fontawesome-free/js/brands';
 
 import '../static/Sidebar.css'
 export default class Sidebar extends Component {
+
+    logout() {
+        $.ajax({
+            url: 'http://localhost:5000/logout',
+            method: 'POST',
+            contentType: 'application/json',
+            headers:{
+                'x-access-token': localStorage.getItem('auth-token')
+            },
+            success: (data) => {
+                console.log('Logout successful');
+            },
+            error: (err) => {
+                console.log(JSON.stringify(err));
+            }
+        })
+        localStorage.removeItem('auth-token');
+        this.props.switchPage('LoginPage');
+    }
+
     render() {
         return (
             <div className="left-nav">
                 <div className="left-nav-item">
-                    <div onClick={() => this.props.switchPage('ApplicationPage')}>
-                        <i className="fas fa-columns left-nav-icon" ></i>
+                    <div className="left-nav-text-wrapper">
+                        <div onClick={() => this.props.switchPage('ApplicationPage')}>
+                            <i className="fas fa-columns left-nav-icon" ></i>
+                        </div>
+                        Apps
                     </div>
-                    <div onClick={() => this.props.switchPage('SearchPage')}>
-                        <i className="fas fa-search left-nav-icon"></i>
+                    <div className="left-nav-text-wrapper">
+                        <div onClick={() => this.props.switchPage('SearchPage')}>
+                            <i className="fas fa-search left-nav-icon"></i>
+                        </div>
+                        Search
+                    </div>
+                    <div className="left-nav-text-wrapper">
+                        <div onClick={() => this.props.switchPage('LoginPage')}>
+                            <i className="fas fa-sign-in-alt left-nav-icon"/>
+                        </div>
+                        Log In
+                    </div>
+                    <div className="left-nav-text-wrapper">
+                        <div onClick={this.logout.bind(this)}>
+                            <i className="fas fa-sign-out-alt left-nav-icon"/>
+                        </div>
+                        Log Out
                     </div>
                 </div>
             </div>
